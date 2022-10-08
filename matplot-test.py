936cc2d4cf0bb1f45ee7139593603a1e3a7a16a5
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-10-08 17:26:37 +0900

    add text

--- matplot-test.py
+++ matplot-test.py
@@ -11,10 +11,20 @@ sample_df = pd.DataFrame( {'x' : x, 'y' : y })
 
 
 ax.plot(sample_df["x"], sample_df["y"])
-ax.axvspan(10, 20, color="gray", alpha=0.3)
+plt.text(0, 150, "0-10", rotation='vertical')
+
+ax.axvspan(10, 20, color="gray", alpha=0.3, label="10-20")
+plt.text(10, 150, "10-20", rotation='vertical')
+
 ax.axvspan(30, 40, color="gray", alpha=0.3)
+plt.text(30, 150, "30-40", rotation='vertical')
+
 ax.axvspan(50, 60, color="gray", alpha=0.3)
+plt.text(50, 150, "50-60", rotation='vertical')
+
 ax.axvspan(70, 80, color="gray", alpha=0.3)
+plt.text(70, 150, "70-80", rotation='vertical')
+
 ax.grid()
 labels = ax.get_xticklabels()
 plt.setp(labels, rotation=90, fontsize=12);
