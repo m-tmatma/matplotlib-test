811fc0c10fdba8a09f80056fa98ef7ca6fd99e6c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-01-15 13:54:46 +0900

    add Poisson Distribution

---
+++ matplot-poisson-test.py
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/51242748/plot-a-poisson-distribution-graph-in-python
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import factorial
+
+lam = 5
+t = np.arange(0, 20, 0.1)
+d = np.exp(-lam)*np.power(lam, t)/factorial(t)
+
+plt.plot(t, d, 'bs')
+plt.grid()
+plt.show()
